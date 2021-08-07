5b06fc66ba6cb01e8a329b19c82fa9f587ed510d
Brandon Ireland <brandonsireland@gmail.com>
2021-08-07 10:10:13 -0700

    feature: base link component

---
+++ components/atoms/BaseLink/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Link from 'next/link';
+import cc from 'classcat';
+
+import { BaseLinkProps } from './BaseLink.types';
+
+// Styles
+import css from './Link.module.scss';
+
+const BaseLink: React.FC<BaseLinkProps> = ({ href, className, children }) => {
+    const linkPattern = /^((http|https|ftp):\/\/)/;
+
+    const classes = cc([
+        css.link,
+        className,
+    ])
+
+    return linkPattern.test(href) ? (
+        <a className={classes} href={href}>
+            {children}
+        </a>
+    ) : (
+        <Link href={href}>
+            <a className={classes}>{children}</a>
+        </Link>
+    );
+};
+
+export default BaseLink;
