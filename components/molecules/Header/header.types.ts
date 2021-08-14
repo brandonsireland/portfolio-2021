10cf074093db0aca7b1339869814ece9854ac9ee
Brandon Ireland <brandonsireland@gmail.com>
2021-08-14 00:07:24 -0700

    feature: header component

---
+++ components/molecules/Header/header.types.ts
@@ -0,0 +1,8 @@
+import { BasePictureProps } from '../../atoms/BasePicture/base-picture.types';
+
+export interface HeaderProps {
+    id: string;
+    title: string;
+    backgroundImage: BasePictureProps;
+    thumbnailImage: BasePictureProps;
+}
