19fd3a8f68b83d0eb169114d088b54c0b98f3a5a
Brandon Ireland <brandonsireland@gmail.com>
2021-08-20 19:46:51 -0700

    feature: parallax children component

---
+++ components/atoms/ParallaxChildren/index.tsx
@@ -0,0 +1,69 @@
+import React, { useRef, useState, useLayoutEffect } from 'react';
+import {
+    useViewportScroll,
+    useTransform,
+    useSpring,
+    motion,
+} from 'framer-motion';
+
+// Types
+import { ParallaxChildrenProps } from './parallax-children.types';
+
+const calculateMinHeight = (height: number, range: number) => {
+    return height + height * range;
+};
+
+const rand = (min = 0, max = 100) => {
+    return Math.floor(Math.random() * (+max - +min)) + +min;
+};
+
+const ParallaxChildren: React.FC<ParallaxChildrenProps> = ({
+    className,
+    children,
+    topOffset = -500,
+    bottomOffset = 500,
+    range = 0.2
+}) => {
+    const { scrollY } = useViewportScroll();
+    const ref = useRef<HTMLDivElement>(null);
+    const [minHeight, setMinHeight] = useState("auto");
+    const [elementTop, setElementTop] = useState(0);
+
+    const springConfig = {
+      damping: 100,
+      stiffness: 100,
+      mass: rand(1, 3)
+    };
+  
+    useLayoutEffect(() => {
+        if (ref.current === null) return;
+        
+        const onResize = () => {
+            setElementTop(ref.current.offsetTop);
+            setMinHeight(calculateMinHeight(ref.current.offsetHeight, range));
+        };
+    
+        onResize();
+        window.addEventListener("resize", onResize);
+        return () => window.removeEventListener("resize", onResize);
+    }, [ref, range]);
+  
+    const y = useSpring(
+      useTransform(
+        scrollY,
+        [elementTop + topOffset, elementTop + bottomOffset],
+        ["0%", `${range * 100}%`]
+      ),
+      springConfig
+    );
+
+    return (
+        <div style={{ minHeight }} className={className}>
+            <motion.div ref={ref} initial={{ y: 0 }} style={{ y }}>
+                {children}
+            </motion.div>
+        </div>
+    );
+};
+
+export default ParallaxChildren;
---
+++ components/atoms/ParallaxChildren/parallax-children.types.ts
@@ -0,0 +1,6 @@
+export interface ParallaxChildrenProps {
+    className?: string;
+    topOffset?: number;
+    bottomOffset?: number;
+    range?: number;
+};
