ef2ea65dbab4c2d087bf85928a71da7c91c99b11
Brandon Ireland <brandonsireland@gmail.com>
2021-08-21 09:48:53 -0700

    feature: footer component

---
+++ components/organisms/Footer/footer.types.ts
@@ -0,0 +1,6 @@
+import { CategoryProps } from "../../molecules/Category/category.types";
+
+export interface FooterProps {
+    id: string;
+    footerItems: CategoryProps[];
+}
--- components/organisms/Footer/index.tsx
+++ components/organisms/Footer/index.tsx
@@ -1,7 +1,69 @@
 import React from 'react';
- 
-const Footer: React.FC = () => {
-    return (<footer>Footer</footer> );
-}
- 
+import { motion } from 'framer-motion';
+
+// Components
+import Category from '../../molecules/Category';
+
+// Types
+import { FooterProps } from './footer.types';
+
+// Style
+import css from './footer.module.scss';
+
+const Footer: React.FC<FooterProps> = ({ id = '', footerItems = [] }) => {
+    const ulVariants = {
+        initial: {
+            transition: { staggerChildren: 0.05, staggerDirection: -1 },
+        },
+        animate: {
+            transition: { staggerChildren: 0.3, delayChildren: 1.6 },
+        },
+    };
+
+    const liVariants = {
+        initial: {
+            y: 50,
+            opacity: 0,
+            transition: {
+                y: { stiffness: 1000 },
+            },
+        },
+        animate: {
+            y: 0,
+            opacity: 1,
+            transition: {
+                y: { stiffness: 1000, velocity: -100 },
+            },
+        },
+    };
+
+    return (
+        <footer id={id} className={css.container}>
+            {footerItems.length > 0 && (
+                <motion.ul
+                    variants={ulVariants}
+                    initial='initial'
+                    animate='animate'
+                    className={css.list}
+                >
+                    {footerItems.map(({ id, value, href }) => (
+                        <motion.li
+                            key={id}
+                            variants={liVariants}
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{
+                                scale: 0.95,
+                                rotate: 360,
+                            }}
+                            className={css.item}
+                        >
+                            <Category href={href} value={value} />
+                        </motion.li>
+                    ))}
+                </motion.ul>
+            )}
+        </footer>
+    );
+};
+
 export default Footer;
