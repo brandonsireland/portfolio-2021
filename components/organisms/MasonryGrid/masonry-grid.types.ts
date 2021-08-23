2ede6fafe54424669f37bbfc7c405c9edaeaaf5c
Brandon Ireland <brandonsireland@gmail.com>
2021-08-23 14:46:06 -0700

    update: masonry has title

--- components/organisms/MasonryGrid/masonry-grid.types.ts
+++ components/organisms/MasonryGrid/masonry-grid.types.ts
@@ -2,5 +2,6 @@ import { BasePictureProps } from '../../atoms/BasePicture/base-picture.types';
 
 export interface MasonryGridProps {
     id: string;
+    title: string;
     masonryPhotos: BasePictureProps[];
 }
