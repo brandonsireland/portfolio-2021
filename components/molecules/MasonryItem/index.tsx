d8ccc2691deba1c70def036d922bebdf85272fa9
Brandon Ireland <brandonsireland@gmail.com>
2021-08-28 13:25:46 -0700

    update: removed lazy animation from masonry item

--- components/molecules/MasonryItem/index.tsx
+++ components/molecules/MasonryItem/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { LazyMotion, domAnimation, m } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 
 // Components
@@ -14,7 +14,7 @@ import BaseVideo from '../../atoms/BaseVideo';
 
 const MasonryItem: React.FC<MasonryItemProps> = ({
     onClick,
-    asset: { id = '', contentType = '' },
+    asset: { id = '', contentType = '', },
     asset,
 }) => {
     const { ref, inView } = useInView({
@@ -22,30 +22,28 @@ const MasonryItem: React.FC<MasonryItemProps> = ({
     });
 
     return (
-        <LazyMotion features={domAnimation}>
-            <m.div
-                ref={ref}
-                layoutId={id}
-                onClick={onClick}
-                initial={{ y: 100, opacity: 0 }}
-                animate={inView && { y: 0, opacity: 1 }}
-                transition={{ duration: 0.5 }}
-                whileHover={{ scale: 1.05, zIndex: 2 }}
-                className={css.item}
-            >
-                {contentType === 'video/mp4' ? (
-                    <BaseVideo
-                        playsInline={true}
-                        autoPlay={true}
-                        muted={true}
-                        loop={true}
-                        url={asset.url}
-                    />
-                ) : (
-                    <BasePicture image={asset} query='?w=373&q=78&fit=fill' />
-                )}
-            </m.div>
-        </LazyMotion>
+        <motion.div
+            ref={ref}
+            layoutId={id}
+            onClick={onClick}
+            initial={{y: 100, opacity: 0}}
+            animate={inView && { y: 0, opacity: 1 }}
+            transition={{ duration: 0.2 }}
+            whileHover={{ scale: 1.05, zIndex: 2 }}
+            className={css.item}
+        >
+            {contentType === 'video/mp4' ? (
+                <BaseVideo
+                    playsInline={true}
+                    autoPlay={true}
+                    muted={true}
+                    loop={true}
+                    url={asset.url}
+                />
+            ): (                    
+                <BasePicture image={asset} query='?w=373&q=78&fit=fill' />
+            )}
+        </motion.div>
     );
 };
 
