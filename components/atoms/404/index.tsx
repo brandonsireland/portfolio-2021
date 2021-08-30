f45360d7568ccb8a18a3cd08ecb86444311fc52e
Brandon Ireland <brandonsireland@gmail.com>
2021-08-30 12:36:53 -0700

    update: 404 component

--- components/atoms/404/index.tsx
+++ components/atoms/404/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import BaseLink from '../BaseLink';
 
 // Styles
 import css from './error.module.scss';
@@ -6,7 +7,14 @@ import css from './error.module.scss';
 const Error: React.FC = () => (
     <section className={css.container}>
         <div className={css.inner}>
-            <h1>404, page doesn&apos;t exist go back.</h1>
+            <span className={css.fourofour}>404</span>
+            <h1>page does not exist, go back.</h1>
+            <div>
+                    <BaseLink href='/' className={css.link}>Go Home</BaseLink>
+                    <BaseLink href='/projects' className={css.link}>
+                        Go To Archive
+                    </BaseLink>
+            </div>
         </div>
     </section>
 );
