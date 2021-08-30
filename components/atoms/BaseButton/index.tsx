e7b099250ce9d281fa41e6023254ad7a178030e1
Brandon Ireland <brandonsireland@gmail.com>
2021-08-30 13:37:18 -0700

    chore: removed unused component

--- components/atoms/BaseButton/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// Styles
-import css from './base-button.module.scss';
-
-const BaseButton: React.FC = ({ children }) => <button className={css.button}>{children}</button>;
-
-export default BaseButton;
