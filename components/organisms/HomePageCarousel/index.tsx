1828f1c64426826323e40ef111d9f19e0b88b941
Brandon Ireland <brandonsireland@gmail.com>
2021-09-03 22:33:05 -0700

    refactor: swapped flickity for swiperjs

--- components/organisms/HomePageCarousel/index.tsx
+++ components/organisms/HomePageCarousel/index.tsx
@@ -1,36 +1,38 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Pagination } from 'swiper';
 
 // Components
-import Carousel from '../../atoms/Carousel';
 import HomePageSlide from '../../molecules/HomePageSlide';
+
 
 // Types
 import { HomePageCarouselProps } from './home-page-carousel.types';
+import { SwiperOptions } from 'swiper';
 
 // Styles
 import css from './home-page-carousel.module.scss';
 
+SwiperCore.use([Pagination]);
+
 const HomePageCarousel: React.FC<HomePageCarouselProps> = ({ slides = [] }) => {
-    const options = {
-        draggable: true,
-        pageDots: false,
-        wrapAround: true,
-        cellAlign: 'center',
-        prevNextButtons: false,
+    const options: SwiperOptions = {
+        slidesPerView: 'auto',
+        loop: true,
+        grabCursor: true,
+        centeredSlides: true,
     };
 
     return (
         <section className={css.container}>
-            <Carousel
-                options={options}
-                itemClassName={css.slide}
-                carouselClassName={css.carousel}
-            >
+            <Swiper {...options}>
                 {slides.length > 0 &&
                     slides.map(slide => (
-                        <HomePageSlide key={slide.id} {...slide} />
+                        <SwiperSlide key={slide.id} className={css.slide}>
+                            <HomePageSlide {...slide} />
+                        </SwiperSlide>
                     ))}
-            </Carousel>
+            </Swiper>
         </section>
     );
 };
