5b07de3998718274fe7f18fef2bc64f0b0cc127b
Brandon Ireland <brandonsireland@gmail.com>
2021-09-09 12:33:18 -0700

    update: mobile carousel

--- components/organisms/HomePageCarousel/index.tsx
+++ components/organisms/HomePageCarousel/index.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import cc from 'classcat';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, { Pagination } from 'swiper';
+import SwiperCore, { Navigation, SwiperOptions } from 'swiper';
 
 // Components
 import HomePageSlide from '../../molecules/HomePageSlide';
-
+import Icon from '../../atoms/Icon';
 
 // Types
 import { HomePageCarouselProps } from './home-page-carousel.types';
-import { SwiperOptions } from 'swiper';
+import { IconType } from '../../atoms/Icon/icon.enums';
 
 // Styles
 import css from './home-page-carousel.module.scss';
 
-SwiperCore.use([Pagination]);
+SwiperCore.use([Navigation]);
 
 const HomePageCarousel: React.FC<HomePageCarouselProps> = ({ slides = [] }) => {
+    const navigationPrevRef = useRef<HTMLDivElement>(null);
+    const navigationNextRef = useRef<HTMLDivElement>(null);
+
     const options: SwiperOptions = {
         slidesPerView: 'auto',
         loop: true,
         grabCursor: true,
         centeredSlides: true,
+        navigation: {
+            prevEl: navigationPrevRef.current!,
+            nextEl: navigationNextRef.current!,
+        },
     };
 
     return (
@@ -32,6 +40,25 @@ const HomePageCarousel: React.FC<HomePageCarouselProps> = ({ slides = [] }) => {
                             <HomePageSlide {...slide} />
                         </SwiperSlide>
                     ))}
+                <div
+                    ref={navigationPrevRef}
+                    className={cc([css.arrow, css.arrowLeft])}
+                >
+                    <Icon
+                        className={css.left}
+                        icon={IconType['Carot']}
+                        alt='Previous Item Button'
+                    />
+                </div>
+                <div
+                    ref={navigationNextRef}
+                    className={cc([css.arrow, css.arrowRight])}
+                >
+                    <Icon
+                        icon={IconType['Carot']}
+                        alt='Previous Item Button'
+                    />
+                </div>
             </Swiper>
         </section>
     );
